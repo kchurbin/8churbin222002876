e7e0f089079f8089f6afb6e3cb86133ff708af6c
kchurbin <149768686+kchurbin@users.noreply.github.com>
2023-11-04 11:22:08 +0200

    whileloop2.java

---
+++ Day2/whileloop2.java
@@ -0,0 +1,13 @@
+public class whileloop2 {
+    public static void main(String[] args) {
+        System.out.println("the above number follows:");
+        int number = 10;
+
+        while (number >= 1) {
+            System.out.println(number);
+            number--;
+        }
+
+        System.out.println("we appreciate");
+    }
+}
