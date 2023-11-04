2da9ea10d838d270c6aac341568ebe6c50992ee5
kchurbin <149768686+kchurbin@users.noreply.github.com>
2023-11-04 11:18:19 +0200

    breakstatement1.java

--- Day2/breakstatement1.java
+++ Day2/breakstatement1.java
@@ -1,4 +1,4 @@
-public class beakCountDownProgram1 {
+public class breakstatement1 {
     public static void main(String[] args) {
         for (int i = 10; i >= 1; i--) {
             System.out.println(i);
