3f2c6517918f2c2edfcc4eaabeb44ea084d82b5a
kchurbin <149768686+kchurbin@users.noreply.github.com>
2023-11-04 11:35:42 +0200

    sumup2.java

--- Day3/sumup2.java
+++ Day3/sumup2.java
@@ -1,7 +1,7 @@
 public class sumup2 {
   static void sums() {
-    int n1 = 3;
-    int n2 = 4;
+    int n1 = 53;
+    int n2 = 24;
     int add = n1 + n2;
     System.out.println("Sum=" + add);
   }
