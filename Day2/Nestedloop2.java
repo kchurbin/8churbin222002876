6ab7df726dcc0986785f140141290a77ff72368e
kchurbin <149768686+kchurbin@users.noreply.github.com>
2023-11-04 10:57:11 +0200

    Update and rename NestedloopMTable2.java to Nestedloop2.java

--- Day2/Nestedloop2.java
+++ Day2/Nestedloop2.java
@@ -1,10 +1,10 @@
-public class NestedloopMTable2 {
+public class Nestedloop2 {
     public static void main(String[] args) {
         int n = 10;
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.print(i * j + "\t");
+            for (int k = 1; k <= 10; k++) {
+                System.out.print(i * k + "\t");
             }
             System.out.println();
         }
