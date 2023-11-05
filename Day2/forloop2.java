1bbfcd2df03e1115c4b58c61dc29b3e6f91d64b0
kchurbin <149768686+kchurbin@users.noreply.github.com>
2023-11-05 08:59:30 +0200

    forloop2.java

--- Day2/forloop2.java
+++ Day2/forloop2.java
@@ -1,7 +1,7 @@
 public class forloop2 {
   public static void main(String[] args) {
     System.out.println("students number on class list follows:");
-    for (int class number = 1; class number < 10; class number++) {
+    for (int class number = 1; class number < 11; class number++) {
       System.out.println(class number);
     }
   }
